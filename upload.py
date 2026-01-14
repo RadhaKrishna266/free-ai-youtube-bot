f22d3b9648b77d7de496425179286ea9ecfc9d8d
RadhaKrishna266 <tejal.patil.168@gmail.com>
2026-01-15 03:20:15 +0530

    Update upload.py

--- upload.py
+++ upload.py
@@ -1,31 +1,4 @@
-import os
-from googleapiclient.discovery import build
-from googleapiclient.http import MediaFileUpload
-from google_auth_oauthlib.flow import InstalledAppFlow
-
-SCOPES = ["https://www.googleapis.com/auth/youtube.upload"]
-
-def upload_video(video_file, title, is_short):
-    flow = InstalledAppFlow.from_client_secrets_file(
-            "client_secret.json", SCOPES
-                )
-                    credentials = flow.run_console()
-
-                        youtube = build("youtube", "v3", credentials=credentials)
-
-                            request = youtube.videos().insert(
-                                    part="snippet,status",
-                                            body={
-                                                        "snippet": {
-                                                                        "title": title,
-                                                                                        "description": title,
-                                                                                                        "categoryId": "27"
-                                                                                                                    },
-                                                                                                                                "status": {
-                                                                                                                                                "privacyStatus": "public"
-                                                                                                                                                            }
-                                                                                                                                                                    },
-                                                                                                                                                                            media_body=MediaFileUpload(video_file)
-                                                                                                                                                                                )
-
-                                                                                                                                                                                    request.execute()
+def upload_video(video_file, title, is_short=False):
+    print("Uploading:", video_file)
+    print("Title:", title)
+    print("Is Short:", is_short)
