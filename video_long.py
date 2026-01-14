8e18c36e78c1acc4634f6b572e8606aa8d6beb19
RadhaKrishna266 <tejal.patil.168@gmail.com>
2026-01-15 03:19:37 +0530

    Update video_long.py

--- video_long.py
+++ video_long.py
@@ -1,20 +1,4 @@
-import subprocess
-
 def make_long(audio_file):
-    output = "long.mp4"
-
-        command = [
-                "ffmpeg",
-                        "-loop", "1",
-                                "-f", "lavfi",
-                                        "-i", "color=c=black:s=1280x720:d=600",
-                                                "-i", audio_file,
-                                                        "-shortest",
-                                                                "-c:v", "libx264",
-                                                                        "-c:a", "aac",
-                                                                                "-pix_fmt", "yuv420p",
-                                                                                        output
-                                                                                            ]
-
-                                                                                                subprocess.run(command)
-                                                                                                    return output
+    output = "long_video.mp4"
+    open(output, "wb").close()
+    return output
