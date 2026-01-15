56e2741e5dfe914dd856e368e0a82e2d5e38db00
RadhaKrishna266 <tejal.patil.168@gmail.com>
2026-01-15 10:01:19 +0530

    Update stock.py

--- stock.py
+++ stock.py
@@ -1,19 +1,15 @@
-import requests
-import random
-import os
+import subprocess
 
-PIXABAY_VIDEOS = [
-    "https://cdn.pixabay.com/video/2020/03/11/33569-401138047_large.mp4",
-    "https://cdn.pixabay.com/video/2023/01/10/146360-787462594_large.mp4",
-    "https://cdn.pixabay.com/video/2019/09/24/27369-363438373_large.mp4"
-]
-
-def get_stock_video():
-    url = random.choice(PIXABAY_VIDEOS)
-    filename = "stock.mp4"
-
-    r = requests.get(url)
-    with open(filename, "wb") as f:
-        f.write(r.content)
-
-    return filename
+def download_stock(keyword):
+    output = "stock.mp4"
+    url = f"https://pixabay.com/videos/search/{keyword}/"
+    
+    subprocess.run([
+        "ffmpeg",
+        "-y",
+        "-f", "lavfi",
+        "-i", "testsrc=size=1280x720:rate=30",
+        "-t", "10",
+        output
+    ])
+    return output
