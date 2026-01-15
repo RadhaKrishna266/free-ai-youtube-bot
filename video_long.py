a5ac3b6ce2aa25bd3784292d9849452cc12d2040
RadhaKrishna266 <tejal.patil.168@gmail.com>
2026-01-15 17:41:34 +0530

    Update video_long.py

--- video_long.py
+++ video_long.py
@@ -1,19 +1,29 @@
 import subprocess
+from stock import download_stock
 
 def make_long(audio_file):
+    stock = download_stock("technology", "stock.mp4")
     output = "long_video.mp4"
 
-    cmd = [
-        "ffmpeg",
-        "-y",
-        "-f", "lavfi",
-        "-i", "color=c=black:s=1280x720:d=180",
-        "-i", audio_file,
-        "-t", "180",
-        "-vf", "scale=1280:720",
-        "-shortest",
-        output
-    ]
+    if stock:
+        cmd = [
+            "ffmpeg", "-y",
+            "-i", stock,
+            "-i", audio_file,
+            "-vf",
+            "scale=1280:720,zoompan=z='min(zoom+0.0003,1.05)':d=1",
+            "-shortest",
+            output
+        ]
+    else:
+        cmd = [
+            "ffmpeg", "-y",
+            "-f", "lavfi",
+            "-i", "color=c=black:s=1280x720:d=300",
+            "-i", audio_file,
+            "-shortest",
+            output
+        ]
 
-    subprocess.run(cmd, check=True)
+    subprocess.run(cmd)
     return output
