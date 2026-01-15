e267c87f9c6f921cac32f777320d4ef0022100ff
RadhaKrishna266 <tejal.patil.168@gmail.com>
2026-01-16 00:13:11 +0530

    Update upload.py

--- upload.py
+++ upload.py
@@ -5,12 +5,29 @@ from googleapiclient.http import MediaFileUpload
 def upload_video(video_file, title, is_short=False):
     api_key = os.environ.get("YOUTUBE_API_KEY")
 
-    youtube = build(
-        "youtube",
-        "v3",
-        developerKey=api_key
-    )
+    from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from google.auth.transport.requests import Request
+import pickle
+import os
 
+SCOPES = ["https://www.googleapis.com/auth/youtube.upload"]
+
+def get_youtube_service():
+    creds = None
+    if os.path.exists("token.pickle"):
+        with open("token.pickle", "rb") as token:
+            creds = pickle.load(token)
+
+    if not creds or not creds.valid:
+        flow = InstalledAppFlow.from_client_secrets_file(
+            "client_secret.json", SCOPES
+        )
+        creds = flow.run_console()
+        with open("token.pickle", "wb") as token:
+            pickle.dump(creds, token)
+
+    return build("youtube", "v3", credentials=creds)
     request = youtube.videos().insert(
         part="snippet,status",
         body={
