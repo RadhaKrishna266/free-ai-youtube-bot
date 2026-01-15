c12d4a457d89d2d466b51f772d2e9ac7c021a175
RadhaKrishna266 <tejal.patil.168@gmail.com>
2026-01-15 09:21:25 +0530

    Update video_long.py

--- video_long.py
+++ video_long.py
@@ -1,4 +1,19 @@
+import subprocess
+
 def make_long(audio_file):
     output = "long_video.mp4"
-    open(output, "wb").close()
+
+    cmd = [
+        "ffmpeg",
+        "-y",
+        "-f", "lavfi",
+        "-i", "color=c=black:s=1280x720:d=360",
+        "-i", audio_file,
+        "-vf",
+        "zoompan=z='min(zoom+0.0003,1.05)':d=1",
+        "-shortest",
+        output
+    ]
+
+    subprocess.run(cmd)
     return output
