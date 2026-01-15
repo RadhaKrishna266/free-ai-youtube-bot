8e79bd28900a737ecbacb705a6dbb5df140bc3eb
RadhaKrishna266 <tejal.patil.168@gmail.com>
2026-01-15 10:02:43 +0530

    Update upload.py

--- upload.py
+++ upload.py
@@ -1,4 +1,26 @@
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
+import os
+
 def upload_video(video_file, title, is_short=False):
-    print("Uploading:", video_file)
-    print("Title:", title)
-    print("Is Short:", is_short)
+    youtube = build(
+        "youtube", "v3",
+        developerKey=os.environ["YOUTUBE_API_KEY"]
+    )
+
+    request = youtube.videos().insert(
+        part="snippet,status",
+        body={
+            "snippet": {
+                "title": title,
+                "description": title,
+                "tags": ["facts", "history", "tech"],
+                "categoryId": "27"
+            },
+            "status": {
+                "privacyStatus": "public"
+            }
+        },
+        media_body=MediaFileUpload(video_file)
+    )
+    request.execute()
