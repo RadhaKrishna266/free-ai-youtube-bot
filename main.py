d7db907c742e45dd6c4df13ef9224ce3b95ed5ca
RadhaKrishna266 <tejal.patil.168@gmail.com>
2026-01-16 17:01:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from topic import get_topic
 from script import generate_scripts
-from voice import generate_voice
+from voice import create_voice
 from video import create_video
 from upload import upload_video
 
@@ -8,8 +8,8 @@ def run():
     topic = get_topic()
     script = generate_scripts(topic)
 
-    short_audio = generate_voice(script, short=True)
-    long_audio = generate_voice(script, short=False)
+    short_audio = create_voice(script, short=True)
+    long_audio = create_voice(script, short=False)
 
     short_video = create_video(short_audio, short=True)
     long_video = create_video(long_audio, short=False)
