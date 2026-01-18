4cb0c334b2804744711bb7cb6a264650593617db
RadhaKrishna266 <tejal.patil.168@gmail.com>
2026-01-18 16:20:39 +0530

    Update upload.py

--- upload.py
+++ upload.py
@@ -7,19 +7,24 @@ from googleapiclient.http import MediaFileUpload
 
 SCOPES = ["https://www.googleapis.com/auth/youtube.upload"]
 
-def upload_video(
-    video_file,
-    title,
-    description,
-    tags,
-    category_id="27",
-    privacy_status="public"
-):
-    credentials = None
+def upload_video(video_path, title, is_short=False):
+    print("Uploading:", video_path)
 
-    if os.path.exists("token.pickle"):
-        with open("token.pickle", "rb") as token:
-            credentials = pickle.load(token)
+    if is_short:
+        category = "short"
+        tags = ["shorts", "facts", "history", "tech"]
+    else:
+        category = "long"
+        tags = ["education", "facts", "history", "technology"]
 
-    if not credentials:
-        flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client
+    # Title handling
+    if is_short:
+        final_title = title[:90]
+    else:
+        final_title = title
+
+    print("Video type:", category)
+    print("Title:", final_title)
+
+    # TODO: YouTube API upload logic here
+
